bfef2c6a6304eef54c376743a24462859a054ee5
CMahk <chandlermahkorn@gmail.com>
2022-05-18 21:51:12 -0700

    Auto agree to installing python3-pip

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt-get install python3-pip
+sudo apt-get -y install python3-pip
 pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
