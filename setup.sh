0ba8cd440901e0e60532dd30be2511accc4077eb
CMahk <chandlermahkorn@gmail.com>
2022-05-18 22:15:09 -0700

    Changed installation order. Torch now uses CPU only

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt-get -y install python3-pip
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 PATH="/users/$USER/.local/bin:$PATH"
 sudo git clone https://github.com/CMahk/yolov5.git
 cd ./yolov5
 pip3 install -r requirements.txt
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
