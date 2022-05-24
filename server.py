b7422315433e73b423bc9ae86aebb5a394572691
cmahk <chandlermahkorn@gmail.com>
2022-05-23 17:39:58 -0700

    server.py now loads the received file and runs the YOLOv5 model on it

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
+import cv2
+import torch
 import os
+from PIL import Image
 
 path = os.path.abspath(os.path.dirname(__file__))
 port = 25565
@@ -18,7 +21,7 @@ print("Server running on " + str(host) + " and port " + str(port))
 while True:
     conn, addr = server.accept()
     print('Got connection from', addr)
-    
+
     with open(path + "/received_file.jpg", "wb") as f:
         print('Opening file...')
         while True:
@@ -28,6 +31,24 @@ while True:
                 break
             f.write(data)
     f.close()
-    
+
     print('Done receiving')
     conn.close()
+    
+    # Model
+    pt = os.path.abspath(os.path.dirname(__file__))
+    ptweight = pt + "/yolov5/fruit_weights/600"
+    print("PATH: " + pt)
+    model = torch.hub.load("ultralytics/yolov5", "custom", path = ptweight, force_reload = False)
+
+    im = pt + "/received_file.jpg"
+
+    # Inference
+    results = model(im)  # includes NMS
+
+    # Results
+    results.print()  
+    results.save()  # or .show()
+
+    results.xyxy[0]  # im1 predictions (tensor)
+    results.pandas().xyxy[0]
